e71a6f770e757b2ff448968c14d1e6a0cbbdeaab
dq5070410 <dq5070410@126.com>
2016-12-12 14:49:44 +0800

    socket api

---
+++ multievent/interface/OSSocket.h
@@ -0,0 +1,331 @@
+/**
+* SOCKET相关操作的API
+* 
+*
+* History
+*	12/11/2012		Created
+*/
+
+#ifndef __OS_SOCKET_H_
+#define __OS_SOCKET_H_
+
+#include "Base.h"
+
+NAME_SPACE_OS_BEGIN
+
+/**
+* 打开一个socket并获得句柄
+* 
+* <iFamily>, 协议族
+* <iType>, 类型
+* <iProtocol>, 协议
+* <bReuseAddr>, 地址是否允许重用
+* <Handle>, 返回的socket句柄
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT SocketOpen(
+	int iFamily,
+	int iType,
+	int iProtocol,
+	ME_HANDLE& Handle );
+
+/**
+* 关闭一个socket
+* 
+* <Handle>, 需要关闭的socket句柄
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT SocketClose( ME_HANDLE Handle );
+
+/**
+* 发送数据
+* 
+* <Handle>, 在哪个socket上发送数据
+* <pszData>, 要发送的数据
+* <dwLength>, 要发送数据的长度
+* <iFlag>, 标记
+*
+* 返回值:
+*		返回本次调用发送的字节数数量
+*/
+int OS_EXPORT SocketSend(
+	ME_HANDLE Handle,
+	const char* pszData,
+	DWORD dwLength,
+	int iFlag = 0 );
+
+/**
+* 发送数据
+* 
+* <Handle>, 在哪个socket上发送数据
+* <pszIov>, 要发送的数据数组
+* <dwCount>, 要发送的数据数组的个数
+*
+* 返回值:
+*		返回本次调用发送的字节数数量
+*/
+int OS_EXPORT SocketSendV(
+	ME_HANDLE Handle,
+	const iovec pszIov[],
+	DWORD dwCount );
+
+/**
+* 收取数据
+* 
+* <Handle>, 在哪个socket上收取数据
+* <pszBuf>, 收取数据的缓冲区
+* <dwLength>, 收取数据的缓冲区大小
+* <iFlag>, 标记
+*
+* 返回值:
+*		返回本次收取的字节数数量
+*/
+int OS_EXPORT SocketRecv(
+	ME_HANDLE Handle,
+	char*& pszBuf,
+	DWORD dwLength,
+	int iFlag = 0 );
+
+/**
+* 发送数据
+* 
+* <Handle>, 在哪个socket上收取数据
+* <pszIov>, 收取数据缓冲区数组
+* <dwCount>, 收取数据缓冲区数组的个数
+*
+* 返回值:
+*		返回本次收取的字节数数量
+*/
+int OS_EXPORT SocketRecvV(
+	ME_HANDLE Handle,
+	iovec szIov[],
+	DWORD dwCount );
+
+/**
+* 发送数据, 仅用于UDP
+* 
+* <Handle>, 在哪个socket上发送数据
+* <pszData>, 要发送的数据
+* <dwLength>, 要发送数据的长度
+* <pPeerAddress>, 目标地址
+* <iFlag>, 标记
+*
+* 返回值:
+*		返回本次调用发送的字节数数量
+*/
+int OS_EXPORT SocketSendTo(
+	ME_HANDLE Handle,
+	const char* pszData,
+	DWORD dwLength,
+	const ME_SOCK_ADDR* pPeerAddress,
+	int iFlag = 0 );
+
+/**
+* 发送数据, 仅用于UDP
+* 
+* <Handle>, 在哪个socket上发送数据
+* <pszIov>, 要发送的数据数组
+* <dwCount>, 要发送的数据数组的个数
+* <pPeerAddress>, 目标地址
+*
+* 返回值:
+*		返回本次调用发送的字节数数量
+*/
+int OS_EXPORT SocketSendVTo(
+	ME_HANDLE Handle,
+	const iovec pszIov[],
+	DWORD dwCount,
+	const ME_SOCK_ADDR* pPeerAddress );
+
+/**
+* 收取数据, 仅用于UDP
+* 
+* <Handle>, 在哪个socket上收取数据
+* <pszBuf>, 收取数据的缓冲区
+* <dwLength>, 收取数据的缓冲区大小
+* <hsaPeerAddress>, 目标地址
+* <iFlag>, 标记
+*
+* 返回值:
+*		返回本次收取的字节数数量
+*/
+int OS_EXPORT SocketRecvFrom(
+	ME_HANDLE Handle,
+	char*& pszBuf,
+	DWORD dwLength,
+	ME_SOCK_ADDR& hsaPeerAddress,
+	int iFlag = 0 );
+
+/**
+* 为一个socket句柄绑定一个本地地址
+* 
+* <Handle>, socket句柄
+* <pLocalAddrYouWantToBind>, 需要绑定的地址
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT SocketBind(
+	ME_HANDLE Handle,
+	const ME_SOCK_ADDR* pLocalAddrYouWantToBind );
+
+/**
+* 屏蔽某个socket的某些功能
+* 
+* <Handle>, socket句柄
+* <iFlag>, 需要屏蔽的功能
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT SocketShutdown(
+	ME_HANDLE Handle,
+	int iFlag );
+
+/**
+* 连接
+* 
+* <Handle>, socket句柄
+* <pPeerAddress>, 要连接到哪里
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT SocketConnect(
+	ME_HANDLE Handle,
+	const ME_SOCK_ADDR* pPeerAddress );
+
+
+/**
+* 监听
+* 
+* <Handle>, socket句柄
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT SocketListen( ME_HANDLE Handle );
+
+/**
+* 监听
+* 
+* <Handle>, socket句柄
+* <hsaPeerAddress>, 要在什么地址、端口上监听
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_HANDLE OS_EXPORT SocketAccept(
+	ME_HANDLE Handle,
+	ME_SOCK_ADDR& hsaPeerAddress );
+
+/**
+* 把一个4字节的DWORD值转换成符合IPV4规范的地址
+* 
+* <dwIP>, IP地址的DWORD值
+*
+* 返回值:
+*		字符串长度为0是失败; 非ME_OK是成功
+*/
+CHikString OS_EXPORT IpDigitalToString( DWORD dwIP );
+
+/**
+* 把一个符合IPV4规范的地址转换成4字节的DWORD值
+* 
+* <pszIP>, IP地址
+*
+* 返回值:
+*		0是失败; 非ME_OK是成功
+*/
+DWORD OS_EXPORT IpStringToDigital( const char* pszIP );
+
+/**
+* 设置socket的I/O模式
+* 
+* <Handle>, 需要设置的socket句柄
+* <iFlag>, 标志
+* <pArg>, 对应于<iFlag>的值
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT IOCtrl(
+	ME_SOCKET Handle,
+	int iFlag,
+	void* pArg );
+
+/**
+* 获取一个已有连接中所绑定的本地地址
+* 
+* <Handle>, 需要设置的socket句柄
+* <hsaLocalAddress>, out, 被绑定的地址
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT SocketGetLocalAddress(
+	ME_HANDLE Handle,
+	ME_SOCK_ADDR& hsaLocalAddress );
+
+/**
+* 获取一个已有连接中所绑定的对端地址
+* 
+* <Handle>, 需要设置的socket句柄
+* <hsaRemoteAddress>, out, 对端地址
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT SocketGetRemoteAddress(
+	ME_HANDLE Handle,
+	ME_SOCK_ADDR& hsaRemoteAddress );
+
+/**
+* 设置socket的属性
+* 
+* <Handle>, 需要设置的socket句柄
+* <iLevel>, 要设置的这个属性被定于哪一个层次
+* <iOption>, 要设置的属性
+* <pOptval>, 设置<iOption>属性的值
+* <iOptlen>, 属性值<pOptval>的长度是多少
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT SocketSetSocketOption(
+	ME_HANDLE Handle,
+	int iLevel, 
+	int iOption, 
+	const void* pOptval, 
+	int iOptlen );
+
+/**
+* 获取socket的属性
+* 
+* <Handle>, 需要设置的socket句柄
+* <iLevel>, 要获取的这个属性被定于哪一个层次
+* <iOption>, 要获取的属性
+* <pOptval>, 设置<iOption>属性的值
+* <iOptlen>, 属性值<pOptval>的长度是多少
+*
+* 返回值:
+*		SOCKET_ERROR是失败; ME_OK是成功
+*/
+ME_Result OS_EXPORT SocketGetSocketOption(
+	ME_HANDLE Handle,
+	int iLevel, 
+	int iOption, 
+	void* pOptval, 
+	int* pOptlen );
+
+NAME_SPACE_OS_END
+
+
+#endif	// __OS_SOCKET_H_
+
+
+
---
+++ multievent/interface/OSThread.h
@@ -0,0 +1,85 @@
+/**
+* 线程相关API
+* 
+*
+* History
+*	12/05/2012		Created
+*/
+
+#ifndef __OS_THREAD_H_
+#define __OS_THREAD_H_
+
+#include "Base.h"
+
+NAME_SPACE_BEGIN
+class CTimeValue;
+NAME_SPACE_END
+
+NAME_SPACE_OS_BEGIN
+
+ME_Result OS_EXPORT ThreadCreate(
+	ME_THREAD_FUNC pFunc,
+	LPVOID pArgs,
+	UINT lFlags,
+	ME_THREAD_ID& htiThreadID,
+	ME_THREAD_HANDLE* hThreadHandle = 0,
+	LONG lPriority = 0,
+	LPVOID pStack = NULL,
+	DWORD dwStacksize = 0 );
+
+/**
+* win32环境：立即结束指定线程
+* linux环境：把线程置为cancel状态(不会立即结束线程而直到下一个cancel点)
+*
+* 在win32环境中调用这个方法，你必须确定你知道要干什么以及这个方法可能会引起
+* 的一些不可预知的后果(比如内存泄漏，句柄泄漏)，如果你不确定，那么不要调用
+* 这个方法
+*
+* 在linux环境中，在可能导致同样的问题的同时，还有一个需要注意的是：如果线程
+* 函数处于循环或者之类的情况下，可能会导致线程迟迟无法退出，因为，cancel点
+* 无法到达，为了避免这个问题，你需要在你的循环里加上ThreadTestDestroy，以确保
+* 在线程能够顺利的到达cancel点
+*/
+ME_Result OS_EXPORT ThreadDestroy(
+	ME_THREAD_HANDLE hThreadHandle, 
+	DWORD dwExitCode );
+
+/**
+* 设置一个cancel点
+*
+* win32环境下，这个函数不做任何处理
+* linux环境下，会设置一cancellation point，这样当线程处于cancel状态时，
+* 它就能够顺利的退出(当然，也还是要花上一点时间)
+*/
+ME_Result OS_EXPORT ThreadTestDestroy();
+
+ME_Result OS_EXPORT ThreadJoin( 
+	ME_THREAD_HANDLE hThreadHandle,
+	CHikTimeValue* pTimeout = NULL,
+	ME_THREAD_STAT* pStat = NULL );
+
+ME_Result OS_EXPORT ThreadSetPriority(
+	ME_THREAD_HANDLE hThreadHandle,
+	int iPriority );
+
+/**
+* 获取当前线程的ID
+*/
+ME_THREAD_ID OS_EXPORT GetThreadID();
+
+/**
+* 获得当前线程的句柄
+*
+* 在linux环境下，ThreadHandle()与ThreadID()的结果是一样的
+* 因为linux没有句柄一说
+*
+* 在win32环境下，则是不同
+*/
+ME_THREAD_HANDLE OS_EXPORT GetThreadHandle();
+
+ME_NAME_SPACE_OS_END
+
+#endif	// __ME_OS_THREAD_H_
+
+
+
